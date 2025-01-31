fba8c032fac690b92c274ee4a995e0fcd6d53187
fahimahammed <fahimahammed.cse@gmail.com>
2025-01-31 22:08:09 +0600

    chore: remove commented code and clean up MinIO setup

--- src/utils/minioClient.ts
+++ src/utils/minioClient.ts
@@ -1,27 +1,3 @@
-// import { Client } from 'minio';
-// import env from '../config/env';
-
-// const minioClient = new Client({
-//     endPoint: env.minio.endpoint,
-//     port: Number(env.minio.port),
-//     useSSL: env.minio.use_ssl,
-//     accessKey: env.minio.access_key,
-//     secretKey: env.minio.secret_key,
-// });
-
-// export const bucketName = 'reels';
-
-// (async () => {
-//     const exists = await minioClient.bucketExists(bucketName).catch(() => false);
-//     if (!exists) {
-//         await minioClient.makeBucket(bucketName, 'us-east-1');
-//         console.log(`Bucket "${bucketName}" created.`);
-//     }
-// })();
-
-// export default minioClient;
-
-
 import { Client } from 'minio';
 import env from '../config/env';
 import logger from './logger';
